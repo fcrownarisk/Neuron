0eb5098f2936074686907d1b2b37cb8809c091aa
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-05 01:09:02 -0700

    Update CPU.c

--- CPU.c
+++ CPU.c
@@ -1,1 +1,21 @@
-
+short main(int argc,char *argv[]){
+   
+}
+int main(){
+   
+}
+float argc(){
+  
+}
+double argv(){
+  
+}
+void init_cell(){
+    function basic_unit(){
+        fx = w1x1 + w2x2 + w3x3
+       
+    }
+}
+void activate_neuron(){
+   function
+}
