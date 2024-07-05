4bcbee04ca4cd9213b7fc6eed5934d14a3089363
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-05 02:36:41 -0700

    Update CPU.c

--- CPU.c
+++ CPU.c
@@ -19,3 +19,38 @@ void init_cell(){
 void activate_neuron(){
    function
 }
+signed iteration(){
+    
+}
+unsigned RETURN(){
+   void R(){
+    int t1,t2;
+   return t1 * sin(R + a) + t2 * cos(R - b);
+   return t1 * cos(R + b) + t2 * sin(R - a);
+   }
+   void E(){
+     int t3,t4;
+   return t3 * asin(R + b) + t4 * acos(R - c);
+   return t3 * acos(R + c) + t4 * asin(R - b);
+   }
+   void T(){
+     int t5,t6;
+   return t5 * tan(T + c) + t6 * atan(T - d); 
+   return t6 * tan(T + d) + t5 * atan(T - c);  
+   }
+   void U(){
+     int t7,t8;
+   return t7 * sinh(U + d) + t8 * cosh(U - d);
+   return t7 * sinh(U + e) + t8 * cosh(U - e);
+   }
+   void R(){
+      int t9,t10;
+   return t9 * ceil(R + e) + t10 * floor(R - e);
+   return t9 * ceil(R + f) + t10 * floor(R - e);
+   }
+   void N(){
+      int t11,t0; 
+   return t11 * (N + f) + t0 * (N - a) 
+   return t11 * (N + a) + t0 * (N - f)
+   }
+}
