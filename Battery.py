883dd04759a52e51e3646881d1439790a53ee070
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-06 03:29:02 -0700

    Add files via upload

---
+++ Battery.py
@@ -0,0 +1,18 @@
+import tkinter
+import Iterator
+import array
+import tuple
+import numpy
+import sys
+import system
+import syslog
+import sysconfig
+def __init__(self):
+    thisx = self.x & self.y | self.z;
+    thisy = self.y & self.x | self.z;
+    thisz = self.z & self.y | self.x;
+
+def Nuclear_Core():
+    c = 299792458;
+    cc = c*c;
+    E = mass * cc;
