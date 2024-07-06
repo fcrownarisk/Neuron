e7c1bc23ed853fbd0b15e8d7578ea7b119e28e67
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-06 05:54:11 -0700

    Update Selfish.py

--- Selfish.py
+++ Selfish.py
@@ -11,7 +11,6 @@ def __init__(self):
     thisx = self.x & self.y | self.z;
     thisy = self.y & self.x | self.z;
     thisz = self.z & self.y | self.x;
-
 def Nuclear_Core():
     c = 299792458;
     cc = c*c;
