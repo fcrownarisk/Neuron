b7438d1c732fc1d24a4026c6f836f5c2b971543a
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-21 09:06:42 +0800

    Update Selfish.py

--- Selfish.py
+++ Selfish.py
@@ -1,12 +1,4 @@
-import tkinter
 import Iterator
-import array
-import tuple
-import numpy
-import sys
-import system
-import syslog
-import sysconfig
 def __init__(self):
     thisx = self.x & self.y | self.z;
     thisy = self.y & self.x | self.z;
