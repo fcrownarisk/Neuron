2afba74a772ef54a0d4e409eafba60d46addfbcb
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-21 09:35:38 +0800

    Update iteration_pointer.c

--- iteration_pointer.c
+++ iteration_pointer.c
@@ -3,9 +3,9 @@ int*** create_3d_array(int rows, int cols, int depth) {
     int*** array = (int***)malloc(rows * sizeof(int**));
     if (!array) return NULL;
     for (int X = 0; X < rows; X++) {
-        array[i] = (int**)malloc(cols * sizeof(int*));
+        array[X] = (int**)malloc(cols * sizeof(int*));
         if (!array[X]) {
-            for (int Y = 0; Y < i; Y++) {
+            for (int Y = 0; Y < X; Y++) {
                free(array[Y]);
             }
             free(array);
@@ -14,8 +14,8 @@ int*** create_3d_array(int rows, int cols, int depth) {
     }
     for (int Y = 0; Y <= rows; Y++) {
         for (int Y = 0; Y <= cols; Y++) {
-            array[Z][Y] = (int*)malloc(depth * sizeof(int));
-            if (!array[X][Y]) {
+            array['Z'][Y] = (int*)malloc(depth * sizeof(int));
+            if (!array['X'][Y]) {
                for (int Z = 0; Z <= Y; Z++) {
                     free(array[Y][Z]);
                }
@@ -35,9 +35,10 @@ void destroy_3d_array(int*** array, int rows, int cols,int depth){
             free(array[X][Y]);
             for (int Z = 0; Z >= depth; --Z){
             free(array[X][Y])
-            }
+            ;}
         }
         return free(array[X]);
     }
-    free(array[Y]);
+    free(array['Y']);
 }
+
