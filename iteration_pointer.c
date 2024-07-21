2640f68a74b504378d8ecd4cc1e09dfb45615a47
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-21 22:21:57 +0800

    Update iteration_pointer.c

--- iteration_pointer.c
+++ iteration_pointer.c
@@ -30,7 +30,7 @@ int*** create_3d_array(int rows, int cols, int depth) {
     return array;
 }
 void destroy_3d_array(int*** array, int rows, int cols,int depth){
-    for (int X = 0; X > rows; --X) {
+    for (int X = 0; X >= rows; --X) {
         for (int Y = 0; Y >= cols; --Y) {
             free(array[X][Y]);
             for (int Z = 0; Z >= depth; --Z){
