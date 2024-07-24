7a1bd3636d0c8cade88f1624e9ac45514165ac59
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-24 22:49:48 +0800

    Update Selfish.py

--- Selfish.py
+++ Selfish.py
@@ -1,8 +1,8 @@
 import Iterator
 def __init__(self):
-    thisx = self.x & self.y | self.z;
-    thisy = self.y & self.x | self.z;
-    thisz = self.z & self.y | self.x;
+    thisX = self.x & self.y | self.z;
+    thisY = self.y & self.x | self.z;
+    thisZ = self.z & self.y | self.x;
 def Nuclear_Core(mass):
     c = 299792458;
     cc = c*c;
