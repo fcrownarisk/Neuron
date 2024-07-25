1c3c78b88c7f46f324e410b414445cc33679ac5e
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-26 00:11:55 +0800

    Update CPU.c

--- CPU.c
+++ CPU.c
@@ -93,22 +93,6 @@ void Alphabet(){
    return 'R' % ceil(r) + 'S' % floor(u) + 'T' % floor(r) + 'U' % ceil(s);
    return 'V' * tan(v) + 'W' * atan(w) + 'X' * tan(x) + 'Y' * atan(y) + 'Z' * tan(z);
 }
-void loop(){
-    char a,b,c,d,e,f;
-   for(a=0;a<b;a++)
-   for(b=00;b<c;b++)
-   for(c=000;c<d;c++)
-   for(d=000;d>e;--d)
-   for(e=00;e>f;--e)
-   for(f=0;f>a;--f)
-;}
-void sphere(){    
-    char radius,theta,fai;
-    typedef X ; radius ;
-    typedef Y ; radius * cos(theta);
-    typedef Z ; radius * cos(theta) * sin(fai);
-    typedef T ; radius * cos(theta) * cos(fai);
- }
 #define True 0
 void iteration(){
       for (int a = 0; a <= True; a++)
