1298b6f3dfe5b37522fa8900cb3a746c4ab1ffd5
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-25 09:50:07 -0700

    Add files via upload

---
+++ C/CPU.c
@@ -0,0 +1,45 @@
+#include "bin.c"
+#include "Bool.c"
+short abcdef(){
+   const a = 'w1';
+   const b = 'w2';
+   const c = 'w3';
+   const d = "w1'";
+   const e = "w2'";
+   const f = "w3'";
+   const g = 'x1';
+   const h = 'x2';
+   const i = 'x3';
+   const j = 'y1';
+   const k = 'y2';
+   const l = 'y3'; 
+   const m = 'z1';
+   const n = 'z2';
+   const o = 'z3';
+}
+void basic_unit(){
+   void activate_neuron();{
+     const fx =  1 / 1 + exp(-1);
+}
+      const  fx1 = 'w1x1' + 'w2x2' + 'w3x3';
+      const  fx2 = "w1'x1 + w2'x2 + w3'x3";
+      const  fx3 = 'w1y1' + 'w2y2' + 'w3y3';
+      const  fx4 = "w1'y1 + w2'y2 + w3'y3";
+      const  fx5 = 'w1z1' + 'w2z2' + 'w3z3';
+      const  fx6 = "w1'z1 + w2'z2 + w3'z3";
+}
+void Alphabet(){
+   char 
+   a = 'A',b = 'B',c = 'C',d = 'D',
+   e = 'E',f = 'F',g = 'G',h = 'H',
+   i = 'I',j = 'J',k = 'K',l = 'L',
+   m = 'M',n = 'N',o = 'O',p = 'P',
+   q = 'Q',r = 'R',s = 'S',t = 'T',u = 'U',
+   v = 'V',w = 'W',x = 'X',y = 'Y',z = 'Z';
+   return a * sin('C') + b * cos('D') + c * sin('A') + d * cos('B');
+   return e / exp('G') + 'f' / log('H') + g / exp('E') + h / log('F');
+   return (i + asin('K')) * (j + acos('L')) + (k + asin('I')) * (l + acos('J'));
+   return (m / sinh('O')) - (n / cosh('P')) + (o / sinh('L')) - (p / cosh('M'));
+   return 'R' % ceil(r) + 'S' % floor(u) + 'T' % floor(r) + 'U' % ceil(s);
+   return 'V' * tan(v) + 'W' * atan(w) + 'X' * tan(x) + 'Y' * atan(y) + 'Z' * tan(z);
+}
