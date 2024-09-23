02aa75b1210a0a39772c70ddcd099ee3e6f65afc
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-23 12:05:07 +0100

    Update bin.c

--- C/bin.c
+++ C/bin.c
@@ -7,3 +7,11 @@ signed bin(){
    int e = "00001111";
    int f = "00010000"; 
 }
+unsigned BIN(){
+   float A = 1/7;
+   float B = 2/7;
+   float C = 3/7;
+   float D = 4/7;
+   float E = 5/7;
+   float F = 6/7;
+}
