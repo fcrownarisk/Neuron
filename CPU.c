cb207ed5207f7dff475bea02aeb25c5630a892fa
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-27 12:12:08 +0100

    Update CPU.c

--- CPU.c
+++ CPU.c
@@ -36,7 +36,7 @@ void Alphabet(){
    q = 'Q',r = 'R',s = 'S',t = 'T',u = 'U',
    v = 'V',w = 'W',x = 'X',y = 'Y',z = 'Z';
    return a * sin('C') + b * cos('D') + c * sin('A') + d * cos('B');
-   return e / exp('G') + 'f' / log('H') + g / exp('E') + h / log('F');
+   return e % exp('G') + f % log('H') + g % exp('E') + h % log('F');
    return (i + asin('K')) * (j + acos('L')) + (k + asin('I')) * (l + acos('J'));
    return (m / sinh('O')) - (n / cosh('P')) + (o / sinh('L')) - (p / cosh('M'));
    return 'R' % ceil(r) + 'S' % floor(u) + 'T' % floor(r) + 'U' % ceil(s);
