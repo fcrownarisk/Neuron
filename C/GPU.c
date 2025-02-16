3959fdce032d74f3ff13431ebc07b46a6d0f9eef
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-02-16 20:40:02 +0800

    Update GPU.c

--- C/GPU.c
+++ C/GPU.c
@@ -2,10 +2,10 @@
 #include "./Vim/ePI.ts"
 #include "./Vim/XYZ.ts"
 void function(){
-const fx1 = 'x^7' + 'w1 * x^3' + 'w2 * x^2' + 'w3 * x' ;
-const fx2 = 'x^7' + "w1' * x^3" - "w2' * x^2" + "w3' * x" ;
-const fx3 = 'y^7' + 'w1 * y^3' + 'w2 * y^2' + 'w3 * y' ;
-const fx4 = 'y^7' + "w1' * y^3" - "w2' * y^2" + "w3' * y" ;
-const fx5 = 'z^7' + 'w1 * z^3' + 'w2 * z^2' + 'w3 * z' ;
-const fx6 = 'z^7' + "w1' * z^3" - "w2' * z^2" + "w3' * z" ;
+const fw1 = 'x^7' + 'w1 * x^3' + 'w2 * x^2' + 'w3 * x' ;
+const fw2 = 'x^7' + "w1' * x^3" - "w2' * x^2" + "w3' * x" ;
+const fw3 = 'y^7' + 'w1 * y^3' + 'w2 * y^2' + 'w3 * y' ;
+const fw4 = 'y^7' + "w1' * y^3" - "w2' * y^2" + "w3' * y" ;
+const fw5 = 'z^7' + 'w1 * z^3' + 'w2 * z^2' + 'w3 * z' ;
+const fw6 = 'z^7' + "w1' * z^3" - "w2' * z^2" + "w3' * z" ;
 }
