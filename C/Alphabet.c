b2e07e5877b7f064626dad5c83c8afce03886c71
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-02-16 20:38:54 +0800

    Create Alphabet.c

---
+++ C/Alphabet.c
@@ -0,0 +1,15 @@
+void Alphabet(){
+   char 
+   a = 'A',b = 'B',c = 'C',d = 'D',
+   e = 'E',f = 'F',g = 'G',h = 'H',
+   i = 'I',j = 'J',k = 'K',l = 'L',
+   m = 'M',n = 'N',o = 'O',p = 'P',
+   q = 'Q',r = 'R',s = 'S',t = 'T',u = 'U',
+   v = 'V',w = 'W',x = 'X',y = 'Y',z = 'Z';
+   return a * sin('C') + b * cos('D') + c * sin('A') + d * cos('B');
+   return e / exp('G') + 'f' / log('H') + g / exp('E') + h / log('F');
+   return (i + asin('K')) * (j + acos('L')) + (k + asin('I')) * (l + acos('J'));
+   return (m / sinh('O')) - (n / cosh('P')) + (o / sinh('L')) - (p / cosh('M'));
+   return 'R' % ceil(r) + 'S' % floor(u) + 'T' % floor(r) + 'U' % ceil(s);
+   return 'V' * tan(v) + 'X' * tan(x) + 'Y' * atan(y) + 'Z' * tan(z);
+}
