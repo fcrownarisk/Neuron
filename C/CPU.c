f0bbc29a3ee445614b7e4ea88b41a431b53220ee
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-02-16 20:38:38 +0800

    Update CPU.c

--- C/CPU.c
+++ C/CPU.c
@@ -19,18 +19,3 @@ void unit(){
       const  fw5 = 'w1z1' + 'w2z2' + 'w3z3';
       const  fw6 = "w1'z1 + w2'z2 + w3'z3";
 }
-void Alphabet(){
-   char 
-   a = 'A',b = 'B',c = 'C',d = 'D',
-   e = 'E',f = 'F',g = 'G',h = 'H',
-   i = 'I',j = 'J',k = 'K',l = 'L',
-   m = 'M',n = 'N',o = 'O',p = 'P',
-   q = 'Q',r = 'R',s = 'S',t = 'T',u = 'U',
-   v = 'V',w = 'W',x = 'X',y = 'Y',z = 'Z';
-   return a * sin('C') + b * cos('D') + c * sin('A') + d * cos('B');
-   return e / exp('G') + 'f' / log('H') + g / exp('E') + h / log('F');
-   return (i + asin('K')) * (j + acos('L')) + (k + asin('I')) * (l + acos('J'));
-   return (m / sinh('O')) - (n / cosh('P')) + (o / sinh('L')) - (p / cosh('M'));
-   return 'R' % ceil(r) + 'S' % floor(u) + 'T' % floor(r) + 'U' % ceil(s);
-   return 'V' * tan(v) + 'X' * tan(x) + 'Y' * atan(y) + 'Z' * tan(z);
-}
